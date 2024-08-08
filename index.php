41d5a11a4e8b1ac187e1d3b174bfff35c02bad6c
AndreaSdr70 <andrea@sdringola.it>
2024-08-08 22:07:50 +0200

    Selfwork PHP 2

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+$text1 = "Marco"; //Questa variabile è stata Corretta
+$text2 = "hai";
+$text3 = "sete"; //Questa variabile è stata Corretta
+$text4 = "?";
+$text5 = "Perchè"; //Questa variabile è stata Corretta
+$text6 = "$text2"; //Questa variabile è stata Corretta
+$text7 = "bevuto"; //Questa variabile è stata Corretta
+$text8 = "tutto";
+
+$text6 = $text2;
+
+$testofinale = "$text1 $text2 $text3 $text4 $text5 $text6 $text7 $text8 ";
+
+echo $testofinale;
+
+
+
+
+?>
